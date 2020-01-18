2f4d0434d43c131fe3a187d9c72ddf2131abe0d3
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2020-01-18 16:11:04 -0500

    commented on and cleaned up script

--- laser chess/boardPieces.gd
+++ laser chess/boardPieces.gd
@@ -2,7 +2,9 @@ extends TileMap
 
 var mouse_pos
 const DESELECT = Vector2(-1,-1)
-var clicked_tile = DESELECT
+var clicked_tile = DESELECT #vector2 location of the clicked tile
+var selected_piece #ID in the tileset of the selected piece
+var active_player
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,12 +21,16 @@ func _input(event):
 	
 	#this event will handle selecting a square for movement or rotation
 	if event is InputEventMouseButton and event.pressed: #when a square is clicked
-		if (clicked_tile == DESELECT): #if there isn't a square seelected
+		#if there isn't a square selected
+		if (clicked_tile == DESELECT):
 			clicked_tile = world_to_map(mouse_pos) #then select the clicked square
-			print(clicked_tile)
 			if get_cellv(clicked_tile) == -1: #if the clicked square is empty
 				clicked_tile = DESELECT #don't select it
 				print(clicked_tile)
+			else:
+				selected_piece = get_cellv(clicked_tile)
+				print(selected_piece)
+		
 		else: #if a square is already selected
 			clicked_tile = DESELECT #deselect it
 			print(clicked_tile)
