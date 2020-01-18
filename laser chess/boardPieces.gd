223ece9ba9c2d0a041d52d073fab3f99027d7a90
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2020-01-18 12:43:43 -0500

    started working on scripts

--- laser chess/boardPieces.gd
+++ laser chess/boardPieces.gd
@@ -1,13 +1,18 @@
 extends TileMap
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var mouse_pos
+var clicked_tile = Vector2(-1,-1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	mouse_pos = get_global_mouse_position()
+	if Input.is_mouse_button_pressed(1):
+		if (clicked_tile == Vector2(-1,-1)):
+			clicked_tile = world_to_map(mouse_pos)
+			print(clicked_tile)
+		else:
+			clicked_tile = Vector2(-1,-1)
